6f165ba3fd881d611fa8579a543418b2c2588f91
catherinegalvez <48770850+catherinegalvez@users.noreply.github.com>
2019-03-20 12:12:17 -0700

    initial commit
    after a couple weeks, this is what we have to show!

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function buttonPress(){
+  document.getElementById("demo").innerHTML="Micheal is not amazing. You know how you are!";
+}
+function Carlos(){
+  alert("Why Not Neil's Advisory")
+}
+
+function Renata(){
+  alert("Why Not Neil's Advisory")
+}
